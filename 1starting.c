67f50a198c6b14eef54a0f32857dde8453d9d9d4
nitin kumar <124438805+Nitinkumar0001@users.noreply.github.com>
2023-02-04 23:18:26 +0530

    Add files via upload

---
+++ 1starting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+	// My first print statement while learning C 
+	printf("Hello World. \n");
+	printf("1st try also going to code and upload \nyou can see my constancy on my github profile .");
+
+	return 0;
+}
